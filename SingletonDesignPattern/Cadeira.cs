f6cbc028359479065fd9e016dd8d56b775b6f4c0
Marcello Guimarães <marcello.gpc@hotmail.com>
2018-12-18 11:22:01 -0200

    Projeto simples implementando o Design Pattern Singleton

---
+++ SingletonDesignPattern/Cadeira.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    public class Cadeira
+    {
+        private static Cadeira instance;
+        public int Qtd { get; set; }
+
+        public static Cadeira Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Cadeira();
+
+                return instance;
+            }
+        }
+
+        private Cadeira() { }
+    }
+}
---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cadeira cadeira_1 = Cadeira.Instance;
+            cadeira_1.Qtd = 5;
+
+            Cadeira cadeira_2 = Cadeira.Instance;
+            cadeira_2.Qtd = 10;
+
+            Console.WriteLine(cadeira_1.Qtd.ToString());
+            Console.WriteLine(cadeira_2.Qtd.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
